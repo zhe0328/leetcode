e5fd050387c214eeec4f04c66788405236e31fe7
Zhe Xu <46717464+zhe0328@users.noreply.github.com>
2022-08-18 16:14:47 +0800

    Time: 4 ms (54.85%), Space: 9.1 MB (72.88%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int end = m-- + n-- - 1;
+        cout << end << endl;
+        while (m >= 0 && n>=0){
+            nums1[end--] = nums1[m] > nums2[n] ? nums1[m--]: nums2[n--];
+        }
+        while (n>=0){
+            nums1[end--] = nums2[n--];
+        }
+    }
+};
