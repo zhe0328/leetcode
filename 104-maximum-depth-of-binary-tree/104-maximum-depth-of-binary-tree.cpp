d9f73774ce779a83bbdc05db16560a2ad416d076
Zhe Xu <46717464+zhe0328@users.noreply.github.com>
2022-09-03 16:59:58 -0400

    Time: 33 ms (6.47%), Space: 18.9 MB (14.27%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.cpp
@@ -14,6 +14,21 @@ public:
     int maxDepth(TreeNode* root) {
         // dfs
         if (root == nullptr) return 0;
-        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+        stack<pair<TreeNode*,int>> stk;
+        stk.push({root,1});
+        int length = 1;
+        while (!stk.empty()){
+            TreeNode* node = stk.top().first;
+            int cur = stk.top().second;
+            stk.pop();
+            if (node->left != nullptr){
+                stk.push({node->left, cur+1});
+            }
+            if (node->right != nullptr){
+                stk.push({node->right, cur+1});
+            }
+            length = cur > length ? cur : length;
+        }
+        return length;
     }
 };
