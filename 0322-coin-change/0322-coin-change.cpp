2dcefd8e5c95ff408abd5336d4815d61233d10fd
Zhe Xu <46717464+zhe0328@users.noreply.github.com>
2022-10-13 10:32:10 -0400

    Time: 242 ms (22.07%), Space: 14.6 MB (56.40%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        sort(coins.begin(), coins.end());
+        int size = coins.size();
+        vector<int> dp(amount+1, amount+1);
+        dp[0] = 0;
+        for (int i=1; i<=amount; i++){
+            for (int j=0; j<size; j++){
+                if (coins[j] <= i){
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                }
+                else {
+                    break;
+                }
+            }
+        }
+        return dp[amount] <= amount ? dp[amount] : -1;
+    }
+};
